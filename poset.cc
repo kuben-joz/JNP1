4ce90d1479bc91d94a10eafa7083666d2c569b76
svn-mimuw-lol-xd <svn-mimuw-lol-xd@yandex.com>
2019-10-28 18:08:18 +0100

    minor ++

--- poset.cc
+++ poset.cc
@@ -2,6 +2,7 @@
 
 namespace jnpzad2{
   using Key = uint64_t;
+  //czy element znaleziony w mapie jest większy od posiadacza mapa
   using Elem = std :: unordered_map<Key , std :: unordered_map<Key, std :: bool>>;
   using ElemMap = std :: unordered_map<Key, Elem>;
   using NameMap = std :: unordered_map<std :: string, Key>;
@@ -74,6 +75,12 @@ bool poset_remove(unsigned long id, char const *value) {
 
 bool poset_add(unsigned long id, char const *value1, char const *value2) {
   //TODO
+  //value1 < value2
+  auto answ1 = posets.find(id);
+  if(answ1 == posets.end()) return 0;
+  auto answ2 = get<0>(answ1);
+
+
 }
 
 bool poset_del(unsigned long id, char const *value1, char const *value2) {
